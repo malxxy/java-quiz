fe20fc9fb7226d397015210d4e5b4a28f37d65c8
malxxy <maliayraguen@gmail.com>
2022-11-21 12:40:30 -0500

    Clean up and condense

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // Variables
-var container = document.querySelector(".container");
+var container = document.getElementById("container");
+var btn = document.querySelectorAll("btn");
 var h1El = document.getElementById("h1-title");
 var startCard = document.querySelector(".opener");
 var start = document.getElementById("start-btn");
@@ -7,13 +8,14 @@ var sectionOne = document.getElementById("section-one");
 var sectionTwo = document.getElementById("section-two");
 var sectionThree = document.getElementById("section-three")
 var GameOver = document.getElementById("game-over")
-var sectionAns = document.getElementById("section-ans");
-var correctAns = document.getElementsByClassName(".correct");
-var incorrectAns = document.getElementsByClassName(".incorrect");
+var correctAns = document.querySelectorAll(".correct");
+var incorrectAns = document.querySelectorAll(".incorrect");
 var btn1 = document.getElementById("first-btn");
 var btn2 = document.getElementById("second-btn");
 var btn3 = document.getElementById("finish-btn");
 var allSections = [sectionOne,sectionTwo,sectionThree];
+
+console.log(correctAns);
 
 // PROMPT FOR LIGHT MODE OR DARK MODE????
  // Set default mode to dark
@@ -32,54 +34,51 @@ var allSections = [sectionOne,sectionTwo,sectionThree];
 //   }
 //  });
 
- // Set default mode for sections as HIDDEN
-//  btn.addEventListener()
-//  var defaultSections = document.querySelectorAll(section);
-//  var setDisplay = "hidden";
-//  if (defaultSections === "hidden") {
-//   defaultSections.setAttribute("style","display:none;");
-//  } else {
-//     defaultSections.setAttribute("style","display:block;");
-//  };
-
 // Style header h1
-h1El.setAttribute("style","background-color: --light; font-family: --headerFont; font-size: 36px; padding: 12px;");
+h1El.setAttribute("style","background-color: --light; font-family: --headerFont; font-size: 36px; padding: 12px; text-align: center;");
 
 // Style start card
 startCard.setAttribute("style",
     "width: 400px; height: 300px; border: --dBlue; background-color: --lBlue;");
 
-// Style start button
-start.setAttribute(
-    "style",
-    "border: black; border-radius: 5%; box-shadow: 5px 3px; padding: 20px; font-size: 30px;");
-
 // Hide start card upon clicking start button and reveal next card
 function hideCard() {
-    startCard.setAttribute("style","display: none;");
-    sectionOne.setAttribute("style","display:block;")
-    // START TIMER COUNTDOWN;
-  }
+  startCard.setAttribute("style","display: none;");
+  sectionOne.setAttribute("style","display:block;")
+  // START TIMER COUNTDOWN;
+}
 start.addEventListener("click",hideCard); // when user clicks on start btn, hide card and go to next section
 
-// Style paragraph answer backgrounds
-sectionAns.children.setAttribute(
-    "style",
-    "background: linear-gradient(90deg, --mTeal 0%, --mGreen 0%, --dTeal 100%);; text:white; padding: 5px;");
+// Hide question sections when webpage is opened
+sectionOne.setAttribute("style","display:none;");
+sectionTwo.setAttribute("style","display:none;");
+sectionThree.setAttribute("style","display:none;");
+GameOver.setAttribute("style","display:none;");
 
-    // function to turn answers opaque
-function opaque(event) {
-    event.currentTarget.setAttribute(
-        "style",
-        "opacity: 50%;"
-    );
-};
-// Event listener when click on ANy of the section answer children p will turn half opaque
-sectionAns.children.addEventListener("hover",opaque);
+// Style paragraph answer backgrounds
+container.setAttribute("style","background: linear-gradient(90deg, --mTeal 0%, --mGreen 0%, --dTeal 100%); text:white; padding: 5px;");
+
+function hideCard1() {
+  sectionOne.setAttribute("style","display: none;");
+  sectionTwo.setAttribute("style","display:block;")
+}
+btn1.addEventListener("click",hideCard1); // when user clicks on next btn, hide card and go to next section
+
+function hideCard2() {
+  sectionTwo.setAttribute("style","display: none;");
+  sectionThree.setAttribute("style","display:block;")
+}
+btn2.addEventListener("click",hideCard2); // when user clicks on next btn, hide card and go to next section
+
+function hideCard3() {
+  sectionThree.setAttribute("style","display: none;");
+  GameOver.setAttribute("style","display:block;")
+}
+btn3.addEventListener("click",hideCard3); // when user clicks on next btn, hide card and go to next section
 
 // Function to change correct answer to green, display "correct!" message and display next button
 function correct(event) {
-    // event.stopPropagation();
+    event.stopPropagation();
     event.currentTarget.setAttribute(
       "style",
       "background-color: --dGreen;"
@@ -87,12 +86,6 @@ function correct(event) {
      // Create element for correct message
      var youRight = document.createElement("Correct!");
      document.section.appendChild(youRight);
-    // NEXT BUTTON APPEARS
-    // if (correct ===true ) {
-    //     var nextBtn = document.createElement(Btn);
-    //     nextBtn.textContent = "Next Question";
-    //     document.body.appendChild(nextBtn);
-    // }
   };
 // When correct answer is clicked on, launch function "correct"
 correctAns.addEventListener("click",correct);
@@ -111,22 +104,3 @@ correctAns.addEventListener("click",correct);
   };
 
 incorrectAns.addEventListener("click",incorrect);
-
-// Function to trigger next section - OPTION 1
-function done1() {
-    sectionOne.setAttribute("style","display: none;");
-    sectionTwo.setAttribute("style","display:block;");
-    };
-// Trigger next section when user selects "Next Question button"
-btn1.addEventListener("click",done1);
-
-// For loop for same process ^^ - OPTION 2s
-for (let index = 0; index < allSections.length; index++) {
-  function done() {
-    section[index].setAttribute("style","display: none;");
-    section[index++].setAttribute("style","display:block;");
-  };
-
-  section[index].btn.addEventListener("click",done);
-};
-
